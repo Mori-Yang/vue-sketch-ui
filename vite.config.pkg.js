8d030219280b677d1d9bd8bbeb797cd4a9491be1
orinyang <orinyang@tencent.com>
2024-12-07 19:46:50 +0800

    bug:can't gen dts when pack

--- vite.config.pkg.js
+++ vite.config.pkg.js
@@ -1,15 +1,30 @@
 import { fileURLToPath, URL } from 'node:url';
 import { resolve } from 'node:path';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import vueDevTools from 'vite-plugin-vue-devtools';
 import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), dts()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      include: ['src/index.ts', 'src/components/**/*', 'src/hooks/**/*', 'src/directives/**/*'],
+      exclude: ['node_modules'],
+      outDir: 'dist',
+      compilerOptions: {
+        sourceMap: false,
+        paths: {
+          '@/*': ['./src/*'],
+          '@@/*': ['./src/components/*'],
+        },
+      },
+      copyDtsFiles: true,
+      pathsToAliases: false,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
