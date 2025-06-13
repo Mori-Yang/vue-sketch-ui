5bfca8155649d02658ad3bff83635b8bb6b8274a
Mori-Yang <ys0514@yeah.net>
2025-06-14 03:29:16 +0800

    chore:test vite config output.exports

--- vite.config.pkg.js
+++ vite.config.pkg.js
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { resolve } from 'node:path';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
